a89f96cd5902993eb7af9c7c2b46f67f67dbc0af
GenericName12345 <neha06@outlook.com>
2019-07-23 11:29:14 -0700

    get_rect

---
+++ classes.py
@@ -0,0 +1,12 @@
+# comment
+# other comment
+
+import pygame
+
+class box:
+    def__init__(self)
+        self.rect = pygame.Rect(8,8,8,100)
+        self.color = pygame.Color(0,0,0,100)
+
+    def colorChange(self):
+        self.color = pygame.Color(255,0,0,100)
