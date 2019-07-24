b60636ad1c7da9216c96696f7ef62bb89c57b3a6
GenericName12345 <neha06@outlook.com>
2019-07-24 10:28:14 -0700

    get_rect

--- Main.py
+++ Main.py
@@ -1,31 +1,48 @@
 # much comment
+# is happening
 
 from classes import *
 import pygame
+from pygame import key
 
 pygame.init()
+clock = pygame.time.Clock()
 
 player = Player()
 objectives = []
-objectives.append(objective())
-objectives.append(objective())
-objectives.append(objective())
-screen = pygame.display.set_mode(1024,720)
+objectives.append(Objective())
+objectives.append(Objective())
+objectives.append(Objective())
+player.box.rect.center = (75, 175)
+objectives[0].box.rect.center = (75, 275)
+screen = pygame.display.set_mode((1024, 720))
+
 
 def update():
+    keys = pygame.key.get_pressed()
+    print(keys[23], keys[24])
+    if keys[23]:
+        player.move(False)
+    if keys[24]:
+        player.move(True)
     for i in objectives:
-        i.handleCollisions(player.box.rect)
+        i.handleCollision(player.box.rect)
+
 
 def render():
-    screen.fill(Color(255,255,255,100))
+    screen.fill(pygame.Color("dark blue"))
     screen.fill(player.box.color, player.box.rect)
     for i in objectives:
         screen.fill(i.box.color, i.box.rect)
 
+
 def main():
-    update()
-    render()
-    pygame.display.flip()
+    while True:
+        clock.tick(60)
+        update()
+        render()
+        pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
--- classes.py
+++ classes.py
@@ -5,24 +5,31 @@ import pygame
 
 class Box:
     def __init__(self):
-        self.rect = pygame.Rect(8,8,8,100)
-        self.color = pygame.Color(0,0,0,100)
+        self.rect = pygame.Rect(50,50,50,50)
+        self.color = pygame.Color("red")
+
 
     def colorChange(self):
         self.color = pygame.Color(255,0,0,100)
-    
-    class Player:
-        def __init__(self):
-            self.box = Box()
-        def move(self, dir):
-            if dir is True:
-                self.box.rect.move_ip(2,0)
-            else:
-                self.box.rect.move_ip(-2,0)
-                
-    class Objective:
-        def __init__(self):
-            self.box = Box()
-        def handleCollision(self,rect):
-            if self.box.rect.colliderect(rect):
-                self.box.changeColor()
+
+
+class Player:
+    def __init__(self):
+        self.box = Box()
+
+
+    def move(self, dir):
+        if dir is True:
+            self.box.rect.move_ip(2,0)
+        else:
+            self.box.rect.move_ip(-2,0)
+
+
+class Objective:
+    def __init__(self):
+        self.box = Box()
+
+
+    def handleCollision(self,rect):
+        if self.box.rect.colliderect(rect):
+            self.box.colorChange()
