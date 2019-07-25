303ed4c2697714f4a9fa878a58e3d7bfd662b094
GenericName12345 <neha06@outlook.com>
2019-07-25 10:39:32 -0700

    get_rect

--- Main.py
+++ Main.py
@@ -1,32 +1,57 @@
 # much comment
 # is happening
 
-from classes import *
-import pygame
-from pygame import key
+import sys, pygame
+from pygame.locals import *
+from classes import Box, Objective, Player
+import random
 
 pygame.init()
 clock = pygame.time.Clock()
-
+screen = pygame.display.set_mode((1024, 720))
 player = Player()
+player.box.rect.center = (75, 175)
 objectives = []
 objectives.append(Objective())
 objectives.append(Objective())
 objectives.append(Objective())
-player.box.rect.center = (75, 175)
+objectives.append(Objective())
+objectives.append(Objective())
+objectives.append(Objective())
 objectives[0].box.rect.center = (75, 275)
-screen = pygame.display.set_mode((1024, 720))
+
+def collidingWithFriends(rect):
+    for i in objectives:
+        if rect.box.rect.colliderect(i.box.rect) and rect is not i:
+            return True
+    return False
+
+
+count = 0
+for i in objectives:
+    i.box.rect.center = ((random.randint(0,1024), random.randint(0,720)))
+    while collidingWithFriends(i) :
+        i.box.rect.center = ((random.randint(0, 1024), random.randint(0, 720)))
+    count += 100
+
+
 
 
 def update():
+    pygame.event.pump()
     keys = pygame.key.get_pressed()
-    print(keys[23], keys[24])
-    if keys[23]:
-        player.move(False)
-    if keys[24]:
-        player.move(True)
+    if keys[pygame.K_LEFT]:
+        player.move(2)
+    if keys[pygame.K_RIGHT]:
+        player.move(1)
+    if keys[pygame.K_UP]:
+        player.move(3)
+    if keys[pygame.K_DOWN]:
+        player.move(4)
+
     for i in objectives:
         i.handleCollision(player.box.rect)
+        i.wait()
 
 
 def render():
@@ -34,7 +59,6 @@ def render():
     screen.fill(player.box.color, player.box.rect)
     for i in objectives:
         screen.fill(i.box.color, i.box.rect)
-
 
 def main():
     while True:
--- classes.py
+++ classes.py
@@ -6,11 +6,11 @@ import pygame
 class Box:
     def __init__(self):
         self.rect = pygame.Rect(50,50,50,50)
-        self.color = pygame.Color("red")
+        self.color = pygame.Color("blue")
 
 
     def colorChange(self):
-        self.color = pygame.Color(255,0,0,100)
+        self.color = pygame.Color(255, 0, 0)
 
 
 class Player:
@@ -19,17 +19,29 @@ class Player:
 
 
     def move(self, dir):
-        if dir is True:
+        if dir is 1:
             self.box.rect.move_ip(2,0)
-        else:
+        if dir is 2:
             self.box.rect.move_ip(-2,0)
+        if dir is 3:
+            self.box.rect.move_ip(0,-2)
+        if dir is 4:
+            self.box.rect.move_ip(0,2)
 
 
 class Objective:
     def __init__(self):
         self.box = Box()
+        self.counter = 121
 
 
     def handleCollision(self,rect):
         if self.box.rect.colliderect(rect):
+            self.counter = 0
+
+    def wait(self):
+        self.counter += 1
+        if self.counter <= 120:
             self.box.colorChange()
+        if self.counter > 120:
+            self.box.color = pygame.Color("blue")
